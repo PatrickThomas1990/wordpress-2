80685fdbdbe24a305a64c39c786eb234db8c0ed7
patrick <patrickthomas76539@gmail.com>
2018-11-20 02:07:30 -0600

    new changes
    Made quite a few changes to the product that had things need to be change

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1542391280;s:7:"authKey";s:64:"$!04{fLS9<*LE0b7N;U0}@e_{_UeOhYi-;<T]4(I1pq{nz@!VwutiLS*;Sm#tePt";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1293;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"$!04{fLS9<*LE0b7N;U0}@e_{_UeOhYi-;<T]4(I1pq{nz@!VwutiLS*;Sm#tePt";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1293;}
